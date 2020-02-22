ecaa1473422c97ee3bdec7595bfec8f0850804f2
Roy Bellingan <admin@seisho.us>
2020-02-22 00:02:24 +0000

    Update and rename define.h to sql.h

--- sql.h
+++ sql.h
@@ -1,8 +1,9 @@
 #ifndef AUX_H
 #define AUX_H
 
+#include <QStringLiteral>
+
 #define QBL(str) QByteArrayLiteral(str)
 #define QSL(str) QStringLiteral(str)
-#define single_row QMap<QByteArray,QByteArray> 
 
 #endif //AUX_H
